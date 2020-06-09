371fdf667045bb50a972db506764072f18d778c9
Xeen0 <sahitsrivyshnav1999@gmail.com>
2020-06-08 22:21:04 -0700

    Updates

---
+++ precache-manifest.77a28f26e289bcc6b96f1c333f4adb29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f3246aafe37bbd107455eb45dbb29a",
+    "url": "/U5A/index.html"
+  },
+  {
+    "revision": "ce83d89c70dd0ea664cc",
+    "url": "/U5A/static/css/main.647e8fc9.chunk.css"
+  },
+  {
+    "revision": "011b0ba378e7e6ff8b34",
+    "url": "/U5A/static/js/2.cf2e647c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/U5A/static/js/2.cf2e647c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce83d89c70dd0ea664cc",
+    "url": "/U5A/static/js/main.8b7e7f41.chunk.js"
+  },
+  {
+    "revision": "ec8878633712a66e497c",
+    "url": "/U5A/static/js/runtime-main.c3634c66.js"
+  },
+  {
+    "revision": "cbae68a77b2c60454afedfbc39254bb7",
+    "url": "/U5A/static/media/drums.cbae68a7.jpg"
+  },
+  {
+    "revision": "7fa6c96a85f5a0674898ba89c710382f",
+    "url": "/U5A/static/media/logo.7fa6c96a.svg"
+  },
+  {
+    "revision": "6a5aef27238d99b892c69831cd9c3f3a",
+    "url": "/U5A/static/media/paint.6a5aef27.jpg"
+  },
+  {
+    "revision": "6759febb8c297e8a7b867bdeebdfec74",
+    "url": "/U5A/static/media/piano.6759febb.jpg"
+  }
+]);
